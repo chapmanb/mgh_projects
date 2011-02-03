d722d2fbd55ff1960bd46d9d90e213acda0274c7
Brad Chapman <chapmanb@50mail.com>
2011-02-02 20:43:07 -0500

    BigWig inferred coverage files and MACS peak calls

--- ms_picher/inferred_coverage_peaks.py
+++ ms_picher/inferred_coverage_peaks.py
@@ -4,7 +4,7 @@
 Driver script that for each experiment:
 - Does bowtie alignments
 - Prepares inferred coverage BED files from paired end alignments
-- Uses BED files to predict peaks
+- Uses BED files to predict peaks with MACS
 - Provides a BigWig inferred coverage display file
 
 Usage:
@@ -12,27 +12,99 @@ Usage:
 """
 import os
 import sys
+import csv
 import subprocess
 
 import yaml
+import pysam
+
+from bcbio.picard import PicardRunner
 
 def main(config_file):
     with open(config_file) as in_handle:
         config = yaml.load(in_handle)
-    align_dir = os.getcwd()
-    for cur_fastq in config["fastq"]:
-        for ref in config["ref"]:
+    align_dir = config["align_dir"]
+    if not os.path.exists(align_dir):
+        os.makedirs(align_dir)
+    picard = PicardRunner(config["program"]["picard"])
+    for ref in config["ref"]:
+        bed_files = {}
+        for cur_fastq in config["fastq"]:
             fastq_one, fastq_two = _get_align_files(cur_fastq, config)
             base = "%s-%s" % (cur_fastq["name"], os.path.basename(ref))
             align_file = bowtie_to_sam(fastq_one, fastq_two, ref, base,
                                        align_dir, config)
             bam_file = sam_to_bam(align_file, fastq_one, fastq_two, ref,
                                   config_file)
-            wig_file = bam_to_wig(bam_file)
+            bed_files[cur_fastq["name"]] = bam_to_bed(bam_file)
+            inferred_bed = generate_inferred_coverage(bam_file)
+            generate_bigwig(inferred_bed, ref, picard)
+        peaks = []
+        for peak in config["peaks"]:
+            peaks.append(call_macs_peaks(bed_files[peak["condition"]],
+                                         bed_files[peak["background"]],
+                                         peak["condition"], ref,
+                                         config["peak_dir"]))
+        print peaks
 
-def bam_to_wig(bam_file):
-    cl = ["bam_to_wiggle.py", bam_file]
+def call_macs_peaks(exp_bed, back_bed, out_base, ref, peak_dir):
+    if not os.path.exists(peak_dir):
+        os.makedirs(peak_dir)
+    genome_size = _size_from_fai(_get_ref_fai(ref))
+    out_name = os.path.join(peak_dir,
+                            "%s-%s-macs" % (out_base, os.path.basename(ref)))
+    cl = ["macs14", "-t", exp_bed, "-c", back_bed, "--name=%s" % out_name,
+          "--format=BED", "--gsize=%s" % genome_size]
     subprocess.check_call(cl)
+    return out_name
+
+def generate_bigwig(bed_file, ref, picard):
+    genome_ref = _get_ref_fai(ref)
+    bam_file = "%s.bam" % os.path.splitext(bed_file)[0]
+    if not os.path.exists(bam_file):
+        cl = ["bedToBam", "-i", bed_file, "-g", genome_ref]
+        with open(bam_file, "wb") as out_handle:
+            subprocess.check_call(cl, stdout=out_handle)
+    sort_bam = picard.run_fn("picard_sort", bam_file)
+    cl = ["bam_to_wiggle.py", sort_bam]
+    subprocess.check_call(cl)
+
+def _size_from_fai(index_file):
+    total = 0
+    with open(index_file) as in_handle:
+        for line in in_handle:
+            parts = line.split()
+            total += int(parts[1])
+    return total
+
+def _get_ref_fai(ref):
+    base_dir, name = os.path.split(ref)
+    return os.path.join(base_dir, os.pardir, "seq", "%s.fa.fai" % name)
+
+def generate_inferred_coverage(bam_file):
+    """Create an inferred coverage BED file from paired end regions.
+    """
+    bed_file = "%s-inferred.bed" % os.path.splitext(bam_file)[0]
+    if not os.path.exists(bed_file):
+        with open(bed_file, "w") as out_handle:
+            writer = csv.writer(out_handle, dialect="excel-tab")
+            samfile = pysam.Samfile(bam_file, "rb")
+            for read in samfile.fetch():
+                if (read.is_paired and read.is_read1 and not read.is_unmapped
+                    and not read.mate_is_unmapped and read.rname == read.mrnm):
+                    chrom = samfile.getrname(read.rname)
+                    pos = [read.pos, read.pos + read.rlen, read.mpos,
+                           read.mpos + read.rlen]
+                    writer.writerow([chrom, min(pos), max(pos), read.qname])
+    return bed_file
+
+def bam_to_bed(bam_file):
+    bed_file = "%s.bed" % os.path.splitext(bam_file)[0]
+    if not os.path.exists(bed_file):
+        with open(bed_file, "w") as out_handle:
+            cl = ["bamToBed", "-i", bam_file]
+            subprocess.check_call(cl, stdout=out_handle)
+    return bed_file
 
 def sam_to_bam(align_file, fastq_one, fastq_two, ref, config_file):
     dir, fname = os.path.split(ref)
